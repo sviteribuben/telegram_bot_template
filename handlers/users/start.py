c44d6a16c219e61d8f899dfba08e5e52c0b1a13f
zvuk <sviteribuben@ya.ru>
2020-12-22 17:42:18 +0300

    HANDLERS/FILTERS_02

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,12 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
-
+from filters import IsPrivate
 from loader import dp
+from re import compile
 
+# "t.me/bot?start=deep_link"
+# "/start deep_link"
 
-@dp.message_handler(CommandStart())
+@dp.message_handler(CommandStart(deep_link=compile(r'\d\d\d')), IsPrivate())
 async def bot_start(message: types.Message):
     await message.answer(f'Привет, {message.from_user.full_name}!')
